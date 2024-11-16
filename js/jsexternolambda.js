1427f347f1ec7b6118050ea3dc463249967dcb7f
YaniryFlorez <ymflorezrivera@gmail.com>
2024-11-16 04:53:08 -0500

    Taller08 Desestructuracion de arreglos y objetos

--- js/jsexternolambda.js
+++ js/jsexternolambda.js
@@ -47,11 +47,11 @@ const conceptosJS = () => {
     */
 
     //guarda al final del arreglo
-    diasSemana.push('Sabado');
-    console.log(diasSemana);
+    //diasSemana.push('Sabado');
+    //console.log(diasSemana);
     //agrega al inicio
-    diasSemana.unshift('Dias');
-    console.log(diasSemana);
+    // diasSemana.unshift('Dias');
+    // console.log(diasSemana);
 
 
     const numerosImpares = [1, 3, 7, 9, 11];
@@ -96,13 +96,13 @@ const conceptosJS = () => {
     console.log(miEstudiante);
 
     //declaraciones de atributos pero como objetos
-    const miProfesor={
-        nombre:"Daniel",
-        apellido:"Ruiz",
-        direccion:{
-            calle:"AV.america",
-            casa:"120",
-            barrio:"libertad"
+    const miProfesor = {
+        nombre: "Daniel",
+        apellido: "Ruiz",
+        direccion: {
+            calle: "AV.america",
+            casa: "120",
+            barrio: "libertad"
 
         }
 
@@ -112,18 +112,130 @@ const conceptosJS = () => {
     console.log(miProfesor.direccion);
     console.log(miProfesor.direccion.calle);
 
-    miProfesor.direccion.calle="Av.10 de agosto";
+    miProfesor.direccion.calle = "Av.10 de agosto";
     console.log(miProfesor);
     console.log(miProfesor.edad);
     console.log(miProfesor.estatura);
 
-    if(miProfesor.edad === miProfesor.estatura){
+    if (miProfesor.edad === miProfesor.estatura) {
         console.log('Son iguales');
-    }else{
+    } else {
         console.log('Estos no son lo mismo');
     }
 
-    
+    //arreglo de objetos
+    const est1 = {
+        nombre: "juan",
+        apellido: "Lujan",
+        edad: 35
+    }
+    const est2 = {
+        nombre: "Daniel",
+        apellido: "Ruiz",
+        edad: 25
+    }
+    const est3 = {
+        nombre: "Luis",
+        apellido: "Castillo",
+        edad: 39
+    }
+    const arregloEstudiantes = [est1, est2, est3];
+
+    console.log(arregloEstudiantes);
+    console.log(arregloEstudiantes[1]);
+
+    console.log(arregloEstudiantes[1].nombre);
+    //mostrar en tablas
+    const arregloEstudiantes2 = [
+        { nombre: "Carlos", apellido: "Tobar", edad: 35 },
+        { nombre: "Anita", apellido: "Lujan", edad: 39 },
+        { nombre: "Andrea", apellido: "Lujan", edad: 29 }
+    ]
+
+    console.log(arregloEstudiantes2);
+
+    console.table(arregloEstudiantes2);
+    //uso del pop
+
+    const estudiante = arregloEstudiantes2.pop();
+    console.log(estudiante);
+    console.table(arregloEstudiantes2);
+
+    //desestructuracion de arreglos
+    //es cuando se tiene un arreglo consiste en poner sus pocisiones en partes
+    //ppor ejemplo el amarillo va ha tener el alias p1
+
+    const colores = ['amarillo', 'azul', 'verde', 'rojo', 'rosado'];
+    const [c1, c2, c3, c4, c5] = colores;
+    console.log(c1);
+    console.log(c5);
+    console.log(c3);
+
+    const [m1, m2, m3, m4, m5, m6, m7] = ['enero', 'febrero', 'marzo', 'abril', 'mayo'];
+    console.log(m1);
+    console.log(m2);
+    console.log(m5);
+    //console.log(m8);
+
+    const [, mes2, , , mes5] = ['enero', 'febrero', 'marzo', 'abril', 'mayo'];
+    console.log(mes2);
+    console.log(mes5);
+
+    //desetructuracion de objetos
+
+    const automovil = {
+        marca: 'Toyota',
+        modelo: 'Pruis',
+        anio: 2024,
+        color: 'plateado'
+    }
+    //aqui se utiliza llaves
+    // aqui los atributos no importa el orden
+    const { marca, modelo, anio, color } = automovil;
+    console.log(modelo);
+    console.log(anio);
+    console.log(color);
+    console.log(marca);
+
+    //desestructuracion con metodo
+    desestructuracion(automovil);
 
 
+    const {nombreP, raza, estatura}= {nombreP:'perro',raza:'pastor',estatura:120}
+    console.log(nombreP);
+    console.log(raza);
+    console.log(estatura);
+
+
+    //declarar un objeto con un atributo complejo (con un articulo de tipo objeto)
+    //y vamos a desestructurar en especial el atributo objeto
+
+    const universidad={
+        nombre:'UCE',
+        estudiante:10000,
+        rector:{
+            nombreR:"Rector1",
+            apellidoR:"ApellidoR"
+        }
+
+    }
+    //desestructurado en dos pasos 
+   /* const {estudiantes, rector}= universidad;
+    const {nombreR, apellidoR}= rector
+    console.log(nombreR);
+    */
+   //desestructurado en un paso
+   const{estudiantes, rector:{nombreR,apellidoR}}= universidad;
+   console.log(nombreR);
+   console.log(apellidoR);
+
+
+}
+//const desestructuracion = (automovil)=>{
+const desestructuracion = ({ marca, modelo, anio, color }) => {
+    console.log('Entro al metodo de desestructuaracion')
+    console.log(modelo);
+    console.log(anio);
+    console.log(color);
+    console.log(marca);
 }
