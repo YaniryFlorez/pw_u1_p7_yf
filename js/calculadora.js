1a93891829d8a6635f4dd2bf3f89ba39d13cd352
YaniryFlorez <ymflorezrivera@gmail.com>
2024-11-22 16:25:20 -0500

    Tarea05 Calculadora mas operaciones

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,49 +1,74 @@
-var num1= 0;
-var num2= 0;
- var op;
+var num1 = "";
+var num2 = "";
+var operacion = "";
+var primerNumeroIngresado = false;
 
 function setearValor(valor) {
-    let valorActual = document.getElementById('id-display').innerText;
-    console.log(valorActual);
     document.getElementById('id-display').innerText = valor;
 }
 
 function setearValorConcatenado(valor) {
     let valorActual = document.getElementById('id-display').innerText;
-    console.log(valorActual);
     document.getElementById('id-display').innerText = valorActual + valor;
 }
 
 function recibirValor(valor) {
-    console.log(valor);
-    this.num1 = valor;
-    console.log(num1);
+    if (!primerNumeroIngresado) {
+        num1 += valor; // Concatenar
+        setearValor(num1); // Muestra el primer número en el display
+    } else {
+        num2 += valor; // Concatenar
+        setearValorConcatenado(valor); // Muestra el segundo número en el display
+    }
 }
 
-function recibirValor2(valor) {
-    console.log(valor);
-    this.num2 = valor;
-    console.log(num2);
-}
-function evaluarOperacion(op){
-    operacion= op;
+function evaluarOperacion(op) {
+    if (num1 !== "") { // Solo si el primer número ha sido ingresado
+        operacion = op;
+        primerNumeroIngresado = true; // Cambiar el estado
+        setearValorConcatenado(" " + operacion + " "); // Muestra el operador en el display
+    }
 }
 
 function igual() {
-    let valorFinal;
- 
-    if (operacion === '+') {
-        valorFinal = num1 + num2
-    } else if (operacion === '-') {
-        valorFinal = num1 - num2
+    if (num1 !== "" && num2 !== "") {
+        let valorFinal;
+
+        const numero1 = parseFloat(num1);
+        const numero2 = parseFloat(num2);
+
+        switch (operacion) {
+            case '+':
+                valorFinal = numero1 + numero2;
+                break;
+            case '-':
+                valorFinal = numero1 - numero2;
+                break;
+            case '*':
+                valorFinal = numero1 * numero2;
+                break;
+            case '/':
+                valorFinal = numero1 / numero2;
+                break;
+            default:
+                valorFinal = "Error";
+        }
+
+        setearValor(valorFinal.toFixed(4)); // Muestra el resultado toFixed(4) muestra las 4 primeras decimas
+        num1 = valorFinal.toString(); // Permite encadenar operaciones
+        num2 = ""; // Reinicia num2
+        operacion = ""; // Reinicia la operación
+        primerNumeroIngresado = false; // Reinicia el estado
     }
-    else if (operacion === 'X') {
-        valorFinal = num1 * num2
-    }
-    else if (operacion === '%') {
-        valorFinal = num1 / num2
-    }
-    setearValor(valorFinal);
 }
- 
- 
+
+function borrar() {
+    // Reinicia todos los valores
+    num1 = "";
+    num2 = "";
+    operacion = "";
+    primerNumeroIngresado = false;
+    setearValor("0"); // Reinicia el display a 0
+}
+
+
